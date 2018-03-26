c1ee2024263cef389be2c721005576f15483a984
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2018-03-26 10:51:47 -0300

    Add base mix project

---
+++ lib/finite_automation.ex
@@ -0,0 +1,18 @@
+defmodule FiniteAutomation do
+  @moduledoc """
+  Documentation for FiniteAutomation.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> FiniteAutomation.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/finite_automation_test.exs
@@ -0,0 +1,8 @@
+defmodule FiniteAutomationTest do
+  use ExUnit.Case
+  doctest FiniteAutomation
+
+  test "greets the world" do
+    assert FiniteAutomation.hello() == :world
+  end
+end
