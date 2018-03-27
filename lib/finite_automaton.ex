bde741d896939a006631e1fd7947ea384fbdd29b
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2018-03-27 20:47:14 -0300

    Refactor function signature with pattern matching

--- lib/finite_automaton.ex
+++ lib/finite_automaton.ex
@@ -12,12 +12,9 @@ defmodule FiniteAutomaton do
     end |> MapSet.new
   end
 
-  def final_states(state, [], _) do
-    MapSet.new [state]
-  end
+  def final_states(state, [], _), do: MapSet.new [state]
 
-  def final_states(state, input, rules) do
-    [head | tail] = input
+  def final_states(state, [head | tail], rules) do
     MapSet.new Enum.flat_map(next_state(state, head, rules), &final_states(&1, tail, rules))
   end
 
