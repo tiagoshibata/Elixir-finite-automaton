7d76510f06d8088956e127f038eceea04ebec07d
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2018-03-27 10:09:49 -0300

    Use atoms for state names

--- lib/finite_automation.ex
+++ lib/finite_automation.ex
@@ -5,7 +5,7 @@ defmodule FiniteAutomation do
     else
       already_visited = MapSet.put(already_visited, state)
       rules_from_state = Enum.filter(rules, &(elem(&1, 0) == state))
-      Enum.flat_map(Enum.filter(rules_from_state, &(elem(&1, 1) == "")), &next_state(elem(&1, 2), input, rules, already_visited))
+      Enum.flat_map(Enum.filter(rules_from_state, &is_nil(elem(&1, 1))), &next_state(elem(&1, 2), input, rules, already_visited))
       ++ (Enum.filter(rules_from_state, &elem(&1, 1) == input)
       |> Enum.map(&elem(&1, 2)))
     end
--- test/finite_automation_test.exs
+++ test/finite_automation_test.exs
@@ -8,13 +8,13 @@ defmodule FiniteAutomationTest do
 
   test "computes next state" do
     rules = [
-      {"q0", "a", "q1"},
-      {"q0", "a", "q2"},
-      {"q0", "", "q3"},
-      {"q3", "", "q0"},
-      {"q3", "a", "q4"},
+      {:q0, :a, :q1},
+      {:q0, :a, :q2},
+      {:q0, nil, :q3},
+      {:q3, nil, :q0},
+      {:q3, :a, :q4},
     ]
-    assert_have_same_elements(FiniteAutomation.next_state("q0", "a", rules),
-    ["q1", "q2", "q4"])
+    assert_have_same_elements(FiniteAutomation.next_state(:q0, :a, rules),
+    [:q1, :q2, :q4])
   end
 end
